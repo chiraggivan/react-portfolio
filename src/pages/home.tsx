33dd6d317faf4fe0feaac1d93362c93236fc9564
Chirag Givan <chirag.givan@gmail.com>
2026-02-07 11:32:08 +0000

    finished adding video connect to play in loop for landing page. just need to write one page for project/ extras for data Analyst/Engineer

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,30 @@
 export default function Home() {
   return (
-    <div className="d-flex flex-column flex-lg-row">
-      <div className="col-lg-1"></div>
-      <div className="col-lg-3">
-        <div className="py-5 m-5">hellow world</div>
+    <div className="d-flex flex-column flex-xl-row">
+      <div className="col-md-0 col-xl-1"></div>
+      <div className="col-xl-3">
+        {/* video with frame will come in the below div */}
+        <div className="py-5 mt-5">
+          <div className="py-xl-5 ps-2 ps-xl-0">
+            <div className="golden-neon-frame">
+              <video autoPlay loop muted playsInline className="portal-video">
+                <source
+                  src="/react-portfolio/videos/react-12sec.webm"
+                  type="video/webm"
+                />
+                <source
+                  src="/react-portfolio/videos/react-12sec.mp4"
+                  type="video/mp4"
+                />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="col-lg-7">
-        <div className="py-5 m-5">
-          <div className="py-5 m-3">
+        <div className="py-xl-5 m-5">
+          <div className="py-xl-5 m-xl-3">
             <h2 className="text-secondary">Full Stack Developer</h2>
             <h1 className="name-heading">Chirag Givan</h1>
             <h3 className="py-3">
